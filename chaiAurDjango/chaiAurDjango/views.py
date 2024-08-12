27cf88cdd214135e1a3509d6309125a471536300
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2024-08-12 01:28:06 +0100

    first commit

---
+++ chaiAurDjango/chaiAurDjango/views.py
@@ -0,0 +1,20 @@
+# Importing HttpResponse from django.http module
+from django.http import HttpResponse
+
+# Importing render function 
+from django.shortcuts import render
+
+# Defining a function named home
+def home(request):
+    # Returning a HttpResponse object with a string "Home page"
+    return render(request, 'index.html')
+
+# Defining a function named about
+def about(request):
+    # Returning a HttpResponse object with a string "About page"
+    return HttpResponse("About page")
+
+# Defining a function named contact
+def contact(request):
+    # Returning a HttpResponse object with a string "Contact page"
+    return HttpResponse("Contact page")
